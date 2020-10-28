48e56b03d951dac6effc2bc3141425a051da181c
crisdev-zero <crisdev-zero@users.noreply.github.com>
2020-10-28 23:49:40 +0000

    deploy: af1a59c6f0772f34e811491ab15e20cb9775152c

---
+++ precache-manifest.ea14fc896968211fbd2ac42d385e1253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572b03fc3da554efcaf4d34b12042b87",
+    "url": "/test-gh-actions/index.html"
+  },
+  {
+    "revision": "34d2a5a43a17c77ecf23",
+    "url": "/test-gh-actions/static/css/main.13ad298f.chunk.css"
+  },
+  {
+    "revision": "ec6470832e73bfdaf37e",
+    "url": "/test-gh-actions/static/js/2.393e8722.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test-gh-actions/static/js/2.393e8722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d2a5a43a17c77ecf23",
+    "url": "/test-gh-actions/static/js/main.b5d1e33a.chunk.js"
+  },
+  {
+    "revision": "d465b9c012fa0b08fe43",
+    "url": "/test-gh-actions/static/js/runtime-main.a6181663.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-gh-actions/static/media/logo.5d5d9eef.svg"
+  }
+]);
